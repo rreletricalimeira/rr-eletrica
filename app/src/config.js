74a872afb725d8fa079981d4de7ddba8f136f678
Robinson <rreletricalimeira@gmail.com>
2026-08-16 22:57:19 -0300

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -2,5 +2,5 @@
 // Veja instruções detalhadas no topo do arquivo src/db/backup.js
 
 export const CONFIG = {
-  GOOGLE_CLIENT_ID: 'COLE_AQUI_SEU_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '1065497422460-7tapibomvdkkl7eja2nvihdpfpfgjoib.apps.googleusercontent.com',
 };
