05a681a9dd533f5821546b78ea78f6ffa668a901
Robinson <rreletricalimeira@gmail.com>
2026-08-17 19:04:11 -0300

    Delete app directory

--- app/src/config.js
+++
@@ -1,6 +0,0 @@
-// config.js — cole aqui o Client ID que você criar no Google Cloud Console.
-// Veja instruções detalhadas no topo do arquivo src/db/backup.js
-
-export const CONFIG = {
-  GOOGLE_CLIENT_ID: '1065497422460-7tapibomvdkkl7eja2nvihdpfpfgjoib.apps.googleusercontent.com',
-};
