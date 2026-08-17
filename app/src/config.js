3e9f03cf58aeecdfe21e15608479922a14663223
Robinson <rreletricalimeira@gmail.com>
2026-08-16 21:02:59 -0300

    Add files via upload

---
+++ app/src/config.js
@@ -0,0 +1,6 @@
+// config.js — cole aqui o Client ID que você criar no Google Cloud Console.
+// Veja instruções detalhadas no topo do arquivo src/db/backup.js
+
+export const CONFIG = {
+  GOOGLE_CLIENT_ID: 'COLE_AQUI_SEU_CLIENT_ID.apps.googleusercontent.com',
+};
